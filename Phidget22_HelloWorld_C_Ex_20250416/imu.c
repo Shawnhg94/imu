6b3373bab12e8c12b1f7dca3677dea1f58b77367
shawn <shawnhg28@gmail.com>
2025-05-01 13:47:03 +0800

    update

--- Phidget22_HelloWorld_C_Ex_20250416/imu.c
+++ Phidget22_HelloWorld_C_Ex_20250416/imu.c
@@ -10,17 +10,14 @@ static void CCONV onAngularRateUpdate(PhidgetGyroscopeHandle ch, void * ctx, con
 }
 
 static void CCONV onSpatialData(PhidgetSpatialHandle ch, void * ctx, const double acceleration[3], const double angularRate[3], const double magneticField[3], double timestamp) {
-	double heading_rad;
 	double heading_degree;
-	
 	printf("Acceleration: \t%lf  |  %lf  |  %lf\n", acceleration[0], acceleration[1], acceleration[2]);
 	printf("AngularRate: \t%lf  |  %lf  |  %lf\n", angularRate[0], angularRate[1], angularRate[2]);
 	printf("MagneticField: \t%lf  |  %lf  |  %lf\n", magneticField[0], magneticField[1], magneticField[2]);
 	printf("Timestamp: %lf\n", timestamp);
 	printf("----------\n");
 
-	heading_rad = atan2(2.0 * (ch->quaternion.w * ch->quaternion.z + ch->quaternion.x * ch->quaternion.y), 1 - 2.0 * (ch->quaternion.y * ch->quaternion.y + ch->quaternion.z * ch->quaternion.z));
-	heading_degree = heading_rad * 180.0 / 3.14159265358979;
+	heading_degree = ch->eulerAngles.heading;
 	printf('Heand: \t%lf', heading_degree);
 }
 
